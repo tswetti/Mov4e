2311c668e7b54e421e0554d88e90c12505957fd7
pety02 <pety02@abv.bg>
2020-05-19 02:05:01 +0300

    Some new tests

--- Mov4eTests/ServiceTests/AllMoviesServiceTests/DummyMockedAllMoviesRepository.cs
+++ Mov4eTests/ServiceTests/AllMoviesServiceTests/DummyMockedAllMoviesRepository.cs
@@ -21,11 +21,26 @@ namespace Mov4eTests.ServiceTests.AllMoviesServiceTests
             new Movie{id=3,title="Star Wars 5",genre=7,pg=0,duration=150,summary="This is the description of the Star Wars 5 movie.",year=new DateTime(1984,4,23),picture=new byte[80]}
         };
 
+        private List<Genre> genres = new List<Genre>
+        {
+            new Genre{id=1, name="Drama"}
+        };
+
+        private List<User> users = new List<User>
+        {
+            new User{id=1, userName="petya", password="myPass"}
+        };
+
+        private List<UserInfo> usersInfo = new List<UserInfo>
+        {
+            new UserInfo{id=1, position="admin"}
+        };
+
         private Movie movie = new Movie{id=2, title = "The Hunger Games: Catching Fire", genre = 7, pg = 14, duration = 180, summary = "This is the description of The Hunger Games: Catching Fire movie.", year = new DateTime(2013, 11, 22), picture = new byte[110] };
 
         public IAllMoviesRepository _allMoviesRepo;
 
-        private Dictionary<int, byte[]> getSortedByAToZMovies()
+        private Dictionary<int, byte[]> GetSortedByAToZMovies()
         {
             Dictionary<int, byte[]> sortedMovies = new Dictionary<int, byte[]>();
             var sortQuery = from m in movies orderby m.title select m;
@@ -36,7 +51,7 @@ namespace Mov4eTests.ServiceTests.AllMoviesServiceTests
             return sortedMovies;
         }
 
-        private Dictionary<int, byte[]> getSortedByOldestMovies()
+        private Dictionary<int, byte[]> GetSortedByOldestMovies()
         {
             Dictionary<int, byte[]> sortedMovies = new Dictionary<int, byte[]>();
             var sortQuery = from m in movies orderby m.year select m;
@@ -45,6 +60,120 @@ namespace Mov4eTests.ServiceTests.AllMoviesServiceTests
                 sortedMovies.Add(item.id, item.picture);
             }
             return sortedMovies;
+        }
+
+        private Dictionary<int, byte[]> GetFilteredByGenreDurationAndPGMovies(int g, int d, int pg)
+        {
+            Dictionary<int, byte[]> filteredMovies = new Dictionary<int, byte[]>();
+            var filterQuery = from m in movies where m.genre == g && m.duration == d && m.pg == pg select new { m.id, m.picture };
+            foreach (var item in filterQuery)
+            {
+                filteredMovies.Add(item.id, item.picture);
+            }
+            return filteredMovies;
+        }
+
+        private Dictionary<int, byte[]> GetFilteredByGenreAndDurationMovies(int g, int d)
+        {
+            Dictionary<int, byte[]> filteredMovies = new Dictionary<int, byte[]>();
+            var filterQuery = from m in movies where m.genre == g && m.duration == d select new { m.id, m.picture };
+            foreach (var item in filterQuery)
+            {
+                filteredMovies.Add(item.id, item.picture);
+            }
+            return filteredMovies;
+        }
+
+        private Dictionary<int, byte[]> GetFilteredByGenreAndPGMovies(int g, int pg)
+        {
+            Dictionary<int, byte[]> filteredMovies = new Dictionary<int, byte[]>();
+            var filterQuery = from m in movies where m.genre == g && m.pg == pg select new { m.id, m.picture };
+            foreach (var item in filterQuery)
+            {
+                filteredMovies.Add(item.id, item.picture);
+            }
+            return filteredMovies;
+        }
+
+        private Dictionary<int, byte[]> GetFilteredByDurationAndPGMovies(int d, int pg)
+        {
+            Dictionary<int, byte[]> filteredMovies = new Dictionary<int, byte[]>();
+            var filterQuery = from m in movies where m.duration == d && m.pg == pg select new { m.id, m.picture };
+            foreach (var item in filterQuery)
+            {
+                filteredMovies.Add(item.id, item.picture);
+            }
+            return filteredMovies;
+        }
+
+        private Dictionary<int, byte[]> GetFilteredByGenreMovies(int g)
+        {
+            Dictionary<int, byte[]> filteredMovies = new Dictionary<int, byte[]>();
+            var filterQuery = from m in movies where m.genre == g select new { m.id, m.picture };
+            foreach (var item in filterQuery)
+            {
+                filteredMovies.Add(item.id, item.picture);
+            }
+            return filteredMovies;
+        }
+
+        private Dictionary<int, byte[]> GetFilteredByDurationMovies(int d)
+        {
+            Dictionary<int, byte[]> filteredMovies = new Dictionary<int, byte[]>();
+            var filterQuery = from m in movies where m.duration == d select new { m.id, m.picture };
+            foreach (var item in filterQuery)
+            {
+                filteredMovies.Add(item.id, item.picture);
+            }
+            return filteredMovies;
+        }
+
+        private Dictionary<int, byte[]> GetFilteredByPGMovies(int pg)
+        {
+            Dictionary<int, byte[]> filteredMovies = new Dictionary<int, byte[]>();
+            var filterQuery = from m in movies where m.pg == pg select new { m.id, m.picture };
+            foreach (var item in filterQuery)
+            {
+                filteredMovies.Add(item.id, item.picture);
+            }
+            return filteredMovies;
+        }
+
+        private Dictionary<int, byte[]> GetMoviesByTitles(List<string> titles)
+        {
+            Dictionary<int, byte[]> moviesDict = new Dictionary<int, byte[]>();
+            for (int i = 0; i < titles.Count; i++)
+            {
+                var filterQuery = from m in movies where m.title == titles[i] select new { m.id, m.picture };
+                foreach (var item in filterQuery)
+                {
+                    moviesDict.Add(item.id, item.picture);
+                }
+            }
+
+            return moviesDict;
+        }
+
+        private Dictionary<int, byte[]> GetAllMovies()
+        {
+            Dictionary<int, byte[]> allMovies = new Dictionary<int, byte[]>();
+            var filterQuery = from m in movies select new { m.id, m.picture };
+            foreach (var item in filterQuery)
+            {
+                allMovies.Add(item.id, item.picture);
+            }
+            return allMovies;
+        }
+
+        private List<string> GetMoviesTitles()
+        {
+            List<string> allMovies = new List<string>();
+            var filterQuery = from m in movies select m.title;
+            foreach (var item in filterQuery)
+            {
+                allMovies.Add(item);
+            }
+            return allMovies;
         }
 
         public DummyMockedAllMoviesRepository()
@@ -88,9 +217,49 @@ namespace Mov4eTests.ServiceTests.AllMoviesServiceTests
                     }
                 });
 
-                mock.Mock<IAllMoviesRepository>().Setup(_allMoviesRepo=>_allMoviesRepo.SortMoviesByTitle()).Returns(getSortedByAToZMovies());
+                mock.Mock<IAllMoviesRepository>().Setup(_allMoviesRepo => _allMoviesRepo.SortMoviesByTitle()).Returns(GetSortedByAToZMovies());
                 
-                mock.Mock<IAllMoviesRepository>().Setup(_allMoviesRepo => _allMoviesRepo.SortByDate()).Returns(getSortedByOldestMovies());
+                mock.Mock<IAllMoviesRepository>().Setup(_allMoviesRepo => _allMoviesRepo.SortByDate()).Returns(GetSortedByOldestMovies());
+
+                mock.Mock<IAllMoviesRepository>().Setup(_allMoviesRepo => _allMoviesRepo.FilterMovies(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<int>()))
+                    .Returns((int g, int d, int pg) => GetFilteredByGenreDurationAndPGMovies(g, d, pg));
+
+                mock.Mock<IAllMoviesRepository>().Setup(_allMoviesRepo => _allMoviesRepo.FilterMoviesByGenresAndDuration(It.IsAny<int>(), It.IsAny<int>()))
+                    .Returns((int g, int d) => GetFilteredByGenreAndDurationMovies(g, d));
+
+                mock.Mock<IAllMoviesRepository>().Setup(_allMoviesRepo => _allMoviesRepo.FilterMoviesByGenresAndPG(It.IsAny<int>(), It.IsAny<int>()))
+                    .Returns((int g, int pg) => GetFilteredByGenreAndPGMovies(g, pg));
+
+                mock.Mock<IAllMoviesRepository>().Setup(_allMoviesRepo => _allMoviesRepo.FilterMoviesByDurationAndPG(It.IsAny<int>(), It.IsAny<int>()))
+                   .Returns((int d, int pg) => GetFilteredByDurationAndPGMovies(d, pg));
+                
+                mock.Mock<IAllMoviesRepository>().Setup(_allMoviesRepo => _allMoviesRepo.FilterMoviesByGenres(It.IsAny<int>()))
+                   .Returns((int g) => GetFilteredByGenreMovies(g));
+
+                mock.Mock<IAllMoviesRepository>().Setup(_allMoviesRepo => _allMoviesRepo.FilterMoviesByDuration(It.IsAny<int>()))
+                    .Returns((int d) => GetFilteredByGenreMovies(d));
+
+                mock.Mock<IAllMoviesRepository>().Setup(_allMoviesRepo => _allMoviesRepo.FilterMoviesByPG(It.IsAny<int>()))
+                   .Returns((int pg) => GetFilteredByGenreMovies(pg));
+                
+                mock.Mock<IAllMoviesRepository>().Setup(_allMoviesRepo => _allMoviesRepo.GetMoviesByTitles(It.IsAny<List<string>>()))
+                    .Returns((List<string> titles) => GetMoviesByTitles(titles));
+
+                mock.Mock<IAllMoviesRepository>().Setup(_allMoviesRepo => _allMoviesRepo.GetMovieTitle(It.IsAny<int>()))
+                    .Returns((int id) => movies.Where(m => m.id == id).Single().title);
+
+                mock.Mock<IAllMoviesRepository>().Setup(_allMoviesRepo => _allMoviesRepo.GetMovies())
+                    .Returns(GetAllMovies());
+
+                mock.Mock<IAllMoviesRepository>().Setup(_allMoviesRepo => _allMoviesRepo.GetMoviesTitles())
+                    .Returns(GetMoviesTitles());
+
+                mock.Mock<IAllMoviesRepository>().Setup(_allMoviesRepo => _allMoviesRepo.GetUserInfo(It.IsAny<string>(), It.IsAny<string>()))
+                    .Returns((string unm, string pass) => new Tuple<int, string>(users.Where(u => u.userName == unm && u.password == pass).Single().id, 
+                    usersInfo.Where(u => u.id == users.Where(us => us.userName == unm && us.password == pass).Single().id).Single().position));
+
+                mock.Mock<IAllMoviesRepository>().Setup(_allMoviesRepo => _allMoviesRepo.GetMovieGenre(It.IsAny<string>()))
+                    .Returns((string gen) => movies.Where(m => m.genre == genres.Where(g => g.name == gen).Single().id).Single().genre);
 
                 _allMoviesRepo = mock.Create<IAllMoviesRepository>();
             }
--- Mov4eTests/ServiceTests/AllMoviesServiceTests/TestAllMoviesService.cs
+++ Mov4eTests/ServiceTests/AllMoviesServiceTests/TestAllMoviesService.cs
@@ -120,5 +120,218 @@ namespace Mov4eTests.ServiceTests.AllMoviesServiceTests
             Assert.AreNotEqual(_iallMoviesService.SortByDate(), null);
             Assert.AreEqual(_iallMoviesService.SortByDate(), expected);
         }
+
+        [Test]
+        public void CorrectFilterMoviesReturnsFilteredMoviesAsADictionaryOfItsIdsAndItsWrappers()
+        {
+            DummyMockedAllMoviesRepository _dummyMockedAllMoviesRepo = new DummyMockedAllMoviesRepository();
+            IAllMoviesService _iallMoviesService = new AllMoviesService(_dummyMockedAllMoviesRepo._allMoviesRepo);
+            Movie expectedMovie = new Movie { id = 2, title = "Terminator", genre = 6, pg = 12, duration = 120, summary = "This is the description of the Terminator movie.", year = new DateTime(1984, 10, 26), picture = new byte[120] };
+            Dictionary<int, byte[]> expected = new Dictionary<int, byte[]>();
+            expected.Add(expectedMovie.id, expectedMovie.picture);
+            Dictionary<int, byte[]> actual = _iallMoviesService.FilterMovies(6, 2, 12);
+
+            Assert.AreNotEqual(_iallMoviesService.FilterMovies(6, 2, 12), null);
+            foreach (var item in actual)
+            {
+                Assert.AreEqual(item.Key, expectedMovie.id);
+                Assert.AreEqual(item.Value, expectedMovie.picture);
+            }
+        }
+
+        [Test]
+        public void CorrectFilterMoviesByGenresAndDurationReturnsFilteredMoviesAsADictionaryOfItsIdsAndItsWrappers()
+        {
+            DummyMockedAllMoviesRepository _dummyMockedAllMoviesRepo = new DummyMockedAllMoviesRepository();
+            IAllMoviesService _iallMoviesService = new AllMoviesService(_dummyMockedAllMoviesRepo._allMoviesRepo);
+            Movie expectedMovie = new Movie { id = 2, title = "Terminator", genre = 6, pg = 12, duration = 120, summary = "This is the description of the Terminator movie.", year = new DateTime(1984, 10, 26), picture = new byte[120] };
+            Dictionary<int, byte[]> expected = new Dictionary<int, byte[]>();
+            expected.Add(expectedMovie.id, expectedMovie.picture);
+            Dictionary<int, byte[]> actual = _iallMoviesService.FilterMoviesByGenresAndDuration(6, 2);
+
+            Assert.AreNotEqual(_iallMoviesService.FilterMoviesByGenresAndDuration(6, 2), null);
+            foreach (var item in actual)
+            {
+                Assert.AreEqual(item.Key, expectedMovie.id);
+                Assert.AreEqual(item.Value, expectedMovie.picture);
+            }
+        }
+
+        [Test]
+        public void CorrectFilterMoviesByGenresAndPGReturnsFilteredMoviesAsADictionaryOfItsIdsAndItsWrappers()
+        {
+            DummyMockedAllMoviesRepository _dummyMockedAllMoviesRepo = new DummyMockedAllMoviesRepository();
+            IAllMoviesService _iallMoviesService = new AllMoviesService(_dummyMockedAllMoviesRepo._allMoviesRepo);
+            Movie expectedMovie = new Movie { id = 2, title = "Terminator", genre = 6, pg = 12, duration = 120, summary = "This is the description of the Terminator movie.", year = new DateTime(1984, 10, 26), picture = new byte[120] };
+            Dictionary<int, byte[]> expected = new Dictionary<int, byte[]>();
+            expected.Add(expectedMovie.id, expectedMovie.picture);
+            Dictionary<int, byte[]> actual = _iallMoviesService.FilterMoviesByGenresAndPG(6, 12);
+
+            Assert.AreNotEqual(_iallMoviesService.FilterMoviesByGenresAndPG(6, 12), null);
+            foreach (var item in actual)
+            {
+                Assert.AreEqual(item.Key, expectedMovie.id);
+                Assert.AreEqual(item.Value, expectedMovie.picture);
+            }
+        }
+
+        [Test]
+        public void CorrectFilterMoviesByDurationAndPGReturnsFilteredMoviesAsADictionaryOfItsIdsAndItsWrappers()
+        {
+            DummyMockedAllMoviesRepository _dummyMockedAllMoviesRepo = new DummyMockedAllMoviesRepository();
+            IAllMoviesService _iallMoviesService = new AllMoviesService(_dummyMockedAllMoviesRepo._allMoviesRepo);
+            Movie expectedMovie = new Movie { id = 2, title = "Terminator", genre = 6, pg = 12, duration = 120, summary = "This is the description of the Terminator movie.", year = new DateTime(1984, 10, 26), picture = new byte[120] };
+            Dictionary<int, byte[]> expected = new Dictionary<int, byte[]>();
+            expected.Add(expectedMovie.id, expectedMovie.picture);
+            Dictionary<int, byte[]> actual = _iallMoviesService.FilterMoviesByDurationAndPG(2, 12);
+
+            Assert.AreNotEqual(_iallMoviesService.FilterMoviesByDurationAndPG(2, 12), null);
+            foreach (var item in actual)
+            {
+                Assert.AreEqual(item.Key, expectedMovie.id);
+                Assert.AreEqual(item.Value, expectedMovie.picture);
+            }
+        }
+
+        [Test]
+        public void CorrectFilterMoviesByGenresReturnsFilteredMoviesAsADictionaryOfItsIdsAndItsWrappers()
+        {
+            DummyMockedAllMoviesRepository _dummyMockedAllMoviesRepo = new DummyMockedAllMoviesRepository();
+            IAllMoviesService _iallMoviesService = new AllMoviesService(_dummyMockedAllMoviesRepo._allMoviesRepo);
+            Movie expectedMovie = new Movie { id = 2, title = "Terminator", genre = 6, pg = 12, duration = 120, summary = "This is the description of the Terminator movie.", year = new DateTime(1984, 10, 26), picture = new byte[120] };
+            Dictionary<int, byte[]> expected = new Dictionary<int, byte[]>();
+            expected.Add(expectedMovie.id, expectedMovie.picture);
+            Dictionary<int, byte[]> actual = _iallMoviesService.FilterMoviesByGenres(6);
+
+            Assert.AreNotEqual(_iallMoviesService.FilterMoviesByGenres(6), null);
+            foreach (var item in actual)
+            {
+                Assert.AreEqual(item.Key, expectedMovie.id);
+                Assert.AreEqual(item.Value, expectedMovie.picture);
+            }
+        }
+
+        [Test]
+        public void CorrectFilterMoviesByDurationReturnsFilteredMoviesAsADictionaryOfItsIdsAndItsWrappers()
+        {
+            DummyMockedAllMoviesRepository _dummyMockedAllMoviesRepo = new DummyMockedAllMoviesRepository();
+            IAllMoviesService _iallMoviesService = new AllMoviesService(_dummyMockedAllMoviesRepo._allMoviesRepo);
+            Movie expectedMovie = new Movie { id = 2, title = "Terminator", genre = 6, pg = 12, duration = 120, summary = "This is the description of the Terminator movie.", year = new DateTime(1984, 10, 26), picture = new byte[120] };
+            Dictionary<int, byte[]> expected = new Dictionary<int, byte[]>();
+            expected.Add(expectedMovie.id, expectedMovie.picture);
+            Dictionary<int, byte[]> actual = _iallMoviesService.FilterMoviesByDuration(2);
+
+            Assert.AreNotEqual(_iallMoviesService.FilterMoviesByDuration(2), null);
+            foreach (var item in actual)
+            {
+                Assert.AreEqual(item.Key, expectedMovie.id);
+                Assert.AreEqual(item.Value, expectedMovie.picture);
+            }
+        }
+
+        [Test]
+        public void CorrectFilterMoviesByPGReturnsFilteredMoviesAsADictionaryOfItsIdsAndItsWrappers()
+        {
+            DummyMockedAllMoviesRepository _dummyMockedAllMoviesRepo = new DummyMockedAllMoviesRepository();
+            IAllMoviesService _iallMoviesService = new AllMoviesService(_dummyMockedAllMoviesRepo._allMoviesRepo);
+            Movie expectedMovie = new Movie { id = 2, title = "Terminator", genre = 6, pg = 12, duration = 120, summary = "This is the description of the Terminator movie.", year = new DateTime(1984, 10, 26), picture = new byte[120] };
+            Dictionary<int, byte[]> expected = new Dictionary<int, byte[]>();
+            expected.Add(expectedMovie.id, expectedMovie.picture);
+            Dictionary<int, byte[]> actual = _iallMoviesService.FilterMoviesByPG(12);
+
+            Assert.AreNotEqual(_iallMoviesService.FilterMoviesByPG(12), null);
+            foreach (var item in actual)
+            {
+                Assert.AreEqual(item.Key, expectedMovie.id);
+                Assert.AreEqual(item.Value, expectedMovie.picture);
+            }
+        }
+
+        [Test]
+        public void CorrectGetMoviesByTitlesReturnsADictionaryOfMoviesIdsAndWrappers()
+        {
+            DummyMockedAllMoviesRepository _dummyMockedAllMoviesRepo = new DummyMockedAllMoviesRepository();
+            IAllMoviesService _iallMoviesService = new AllMoviesService(_dummyMockedAllMoviesRepo._allMoviesRepo);
+            Movie expectedMovie = new Movie { id = 1, title = "Titanic", genre = 1, pg = 0, duration = 210, summary = "This is the description of the Titanic movie.", year = new DateTime(1997, 11, 1), picture = new byte[100] };
+            Dictionary<int, byte[]> expected = new Dictionary<int, byte[]>();
+            expected.Add(expectedMovie.id, expectedMovie.picture);
+            Dictionary<int, byte[]> actual = _iallMoviesService.GetMoviesByTitles(new List<string> { expectedMovie.title });
+
+            Assert.AreNotEqual(_iallMoviesService.GetMoviesByTitles(new List<string> { expectedMovie.title }), null);
+            foreach (var item in expected)
+            {
+                Assert.AreEqual(item.Key, 1);
+                Assert.AreEqual(item.Value, new byte[100]);
+            }
+        }
+
+        [Test]
+        public void CorrectGetMoviesTitleReturnsTheTitleOfTheMovieByItsId()
+        {
+            DummyMockedAllMoviesRepository _dummyMockedAllMoviesRepo = new DummyMockedAllMoviesRepository();
+            IAllMoviesService _iallMoviesService = new AllMoviesService(_dummyMockedAllMoviesRepo._allMoviesRepo);
+            Movie expectedMovie = new Movie { id = 1, title = "Titanic", genre = 1, pg = 0, duration = 210, summary = "This is the description of the Titanic movie.", year = new DateTime(1997, 11, 1), picture = new byte[100] };
+
+            Assert.AreNotEqual(_iallMoviesService.SetMovieTitle(1), null);
+            Assert.AreEqual(_iallMoviesService.SetMovieTitle(1), "Titanic");
+        }
+
+        [Test]
+        public void CorrectGetMoviesReturnsADictionaryOfItsIdsAndWrappers()
+        {
+            DummyMockedAllMoviesRepository _dummyMockedAllMoviesRepo = new DummyMockedAllMoviesRepository();
+            IAllMoviesService _iallMoviesService = new AllMoviesService(_dummyMockedAllMoviesRepo._allMoviesRepo);
+            Movie expectedMovie1 = new Movie { id = 1, title = "Titanic", genre = 1, pg = 0, duration = 210, summary = "This is the description of the Titanic movie.", year = new DateTime(1997, 11, 1), picture = new byte[100] };
+            Movie expectedMovie2 = new Movie { id = 2, title = "Terminator", genre = 6, pg = 12, duration = 120, summary = "This is the description of the Terminator movie.", year = new DateTime(1984, 10, 26), picture = new byte[120] };
+            Movie expectedMovie3 = new Movie { id = 3, title = "Star Wars 5", genre = 7, pg = 0, duration = 150, summary = "This is the description of the Star Wars 5 movie.", year = new DateTime(1984, 4, 23), picture = new byte[80] };
+            Dictionary<int, byte[]> dict = new Dictionary<int, byte[]>();
+            dict.Add(expectedMovie1.id, expectedMovie1.picture);
+            dict.Add(expectedMovie2.id, expectedMovie2.picture);
+            dict.Add(expectedMovie3.id, expectedMovie3.picture);
+
+            Assert.AreNotEqual(_iallMoviesService.SetMoviesList(), null);
+            foreach(var item in dict)
+            {
+                foreach(var el in _iallMoviesService.SetMoviesList())
+                {
+                    Assert.AreEqual(item.Key, el.Key);
+                    Assert.AreEqual(item.Value, el.Value);
+                }
+            } 
+        }
+
+        [Test]
+        public void CorrectGetMoviesTitlesReturnsAListOfMoviesTitlesFromDB()
+        {
+            DummyMockedAllMoviesRepository _dummyMockedAllMoviesRepo = new DummyMockedAllMoviesRepository();
+            IAllMoviesService _iallMoviesService = new AllMoviesService(_dummyMockedAllMoviesRepo._allMoviesRepo);
+            List<string> expected = new List<string> {"Titanic", "Terminator", "Star Wars 5"};
+
+            Assert.AreNotEqual(_iallMoviesService.GetMovieTitles(), null);
+            for (int i=0; i<expected.Count; i++)
+            {
+                Assert.AreEqual(_iallMoviesService.GetMovieTitles()[i], expected[i]);
+            }
+        }
+
+        [Test]
+        public void CorrectGetUserInfoReturnsATupleFromUsersIdAndPosition()
+        {
+            DummyMockedAllMoviesRepository _dummyMockedAllMoviesRepo = new DummyMockedAllMoviesRepository();
+            IAllMoviesService _iallMoviesService = new AllMoviesService(_dummyMockedAllMoviesRepo._allMoviesRepo);
+
+            Assert.AreNotEqual(_iallMoviesService.GetUserInfo("petya", "myPass"), null);
+            Assert.AreEqual(_iallMoviesService.GetUserInfo("petya", "myPass"), new Tuple<int, string>(1, "admin"));
+        }
+
+        [Test]
+        public void CorrectGetMovieGenreReturnsTheMovieGenreAsIntNumber()
+        {
+            DummyMockedAllMoviesRepository _dummyMockedAllMoviesRepo = new DummyMockedAllMoviesRepository();
+            IAllMoviesService _iallMoviesService = new AllMoviesService(_dummyMockedAllMoviesRepo._allMoviesRepo);
+
+            Assert.AreNotEqual(_iallMoviesService.GetMovieGenre("Drama"), null);
+            Assert.AreEqual(_iallMoviesService.GetMovieGenre("Drama"), 1);
+        }
     }
 }
